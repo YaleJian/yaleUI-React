0b16b832c9843c82738ef3a4c4b3c26f8a3a53ed
yalejian <yalejian@gmail.com>
2019-10-17 16:12:25 +0800

    基于腾讯云对象存储webpack自动打包部署

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -1,4 +1,36 @@
+const path = require('path');
+const CosPlugin = require('cos-webpack');
+function resolve(dir) {
+    return path.join(__dirname, '.', dir)
+}
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
+    //使用@绝对路径
+    config.resolve.alias = {
+        '@': resolve('src')
+    };
+
+    //引用腾讯云对象存储源，加速静态资源下载，不占用本机服务器带宽
+    if(process.env.NODE_ENV === 'production') {
+        config.output.publicPath = 'https://homepage-1252187891.cos.ap-beijing.myqcloud.com/build';
+    }
+
+    // 去除css，js名称中的哈希
+    config.output.filename = 'static/js/[name].js';
+    config.output.chunkFilename = 'static/js/[name].chunk.js';
+    config.plugins[5].options.filename = "static/css/[name].css";
+    config.plugins[5].options.chunkFilename = "static/css/[name].chunk.css";
+
+    //上传到腾讯云cos对象存储
+    config.plugins.push(
+        new CosPlugin({  // 配置 Plugin
+            secretId: 'AKIDOcekgT9aszue7RTTjopjSLONBnEWraqw',
+            secretKey: '4xjKp8Fvb9XkqpdGMH1Zv66ucEksoeJn',
+            bucket: 'homepage-1252187891',  // COS 存储对象名称，格式为对象名称加应用ID (APPID)，如： bucket-1250000000
+            region: 'ap-beijing',  // COS 存储地域
+            // exclude: /index.html$/,  // 可选，排除特定文件，正则表达式，如: /index.html$/
+            path: '/'  // 存储路径， 默认为 [hash]
+        })
+    );
+
     return config;
 };
