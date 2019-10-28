5f7f1abb074bceaf4743b11a5c87f30d8a026965
yalejian <yalejian@gmail.com>
2019-10-28 11:28:51 +0800

    修复

--- react/src/modules/utils/Axios.js
+++ react/src/modules/utils/Axios.js
@@ -1,41 +1,46 @@
-import axios from 'axios'
+import React from "react";
+import axios from 'axios';
 import Progress from '../Progress/Progress'
+import Message from "../message/Message";
 // 设置超时时间
 axios.defaults.timeout = 10000;
+
+//请求前
 axios.interceptors.request.use(config => {
     // 请求之前加loading
     Progress.start();
     return config
 }, error => {
-    alert(error);
     if (error.response) {
         // 请求已发出，但服务器响应的状态码不在 2xx 范围内
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+        Message(<div>
+            <div>Error: {error.response.status}</div>
+            <div>Url: {error.response.config.url}</div>
+        </div>);
     } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
+        Message(error.message);
     }
-    console.log(error.config);
     return Promise.reject(error)
 });
+
+//请求后
 axios.interceptors.response.use(config => {
     // 响应成功关闭loading
     Progress.done();
     return config
 }, error => {
-    alert(error);
+
     if (error.response) {
         // 请求已发出，但服务器响应的状态码不在 2xx 范围内
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+        Message(<div>
+            <div>Error: {error.response.status}</div>
+            <div>Url: {error.response.config.url}</div>
+        </div>);
     } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
+        Message(error.message);
     }
-    console.log(error.config);
     return Promise.reject(error)
 });
 export default axios;
