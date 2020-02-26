24cf17dbf9e0c0eba7c5453b1eca13e656c539e8
jianzeyang <12345678>
2020-02-26 19:39:54 +0800

    备案变更

--- react/src/pages/Desktop/Desktop.js
+++ react/src/pages/Desktop/Desktop.js
@@ -24,16 +24,16 @@ class Desktop extends React.Component {
         let data = [
             {name: "APP下载", link: "appDownload", icon: "i-ziyuan", bg: "grey"},
             {name: "Yale UI", link: "ui", icon: "i-uicn", bg: "grey"},
-            {name: "摄影展", link: "photo", icon: "i-chongwusheying", bg: ""},
+            {name: "摄影", link: "photo", icon: "i-chongwusheying", bg: ""},
             {name: "文章", link: "pic", icon: "i-book-2", bg: "green"},
             {name: "天气", link: "weather", icon: "i-duoyun1", bg: "white"},
-            {name: "云盘", link: "pic", icon: "i-yunpan2", bg: "grey"},
+            {name: "我的云盘", link: "pic", icon: "i-yunpan2", bg: "grey"},
             {name: "今天吃什么", link: "eatToday", icon: "i-zuofan", bg: ""},
             {name: "账号设置", link: "pic", icon: "i-anquan", bg: "orange"},
             {name: "后台管理", link: "pic", icon: "i-huabanfuben1", bg: "blue"},
-            {name: "留言反馈", link: "pic", icon: "i-fankui1", bg: "grey"},
-            {name: "关于我们", link: "pic", icon: "i-guanyu-", bg: "grey"},
-            {name: "华富云", link: "hfm", icon: "i-guanggaopai", bg: "white"},
+            {name: "反馈", link: "pic", icon: "i-fankui1", bg: "grey"},
+            {name: "关于我", link: "pic", icon: "i-guanyu-", bg: "grey"},
+            // {name: "华富云", link: "hfm", icon: "i-guanggaopai", bg: "white"},
         ];
         let apps = data.map((app) => {
             return <Link to={app.link} key={app.name}>
@@ -42,7 +42,7 @@ class Desktop extends React.Component {
             </Link>
         });
         return <>
-            <Header children={"扬歌YaleJian"}/>
+            <Header children={"扬歌的分享"}/>
             <div className={"ya-homePage"}>
                 <div className={"ya-h-search"}>
                     <Input type="search" className="ya-h-search-input right" placeholder={"搜索网页"}
