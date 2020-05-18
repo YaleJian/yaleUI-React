4b9a6cd7b0364225d3f826c67f8c22234a7e51bb
jianzeyang <12345678>
2020-05-18 10:55:15 +0800

    V0.1.1 代码结构调整

--- src/pages/common/Footer.js
+++ src/pages/common/Footer.js
@@ -5,9 +5,8 @@ class Footer extends React.Component {
     render() {
         return <div className={"ya-footer"}>
             © 2020 扬歌YaleJian
-            | <a className={"copyright"} href="http://www.beian.miit.gov.cn" rel="noopener noreferrer"
-                 target="_blank">苏ICP备18013414号</a>
-            | <img className={"publicSafety"} src={"http://www.beian.gov.cn/img/ghs.png"} alt={""}/>
+            | <a className={"copyright"} href="http://www.beian.miit.gov.cn" target="_blank">苏ICP备18013414号</a>
+            | {/*<img className={"publicSafety"} src={"https://www.beian.gov.cn/img/ghs.png"} alt={""}/>*/}
             <a className={"copyright"}
                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011702000196"
                rel="noopener noreferrer" target="_blank">苏公网安备32011702000196号</a>
